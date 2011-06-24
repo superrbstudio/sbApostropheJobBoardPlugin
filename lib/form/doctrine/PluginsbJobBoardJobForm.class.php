3e677ea97bc3fb4ba77a6fb7be0d4d5beea081e1
Giles Smith <giles@superrb.com>
2011-06-24 11:14:51 +0100

    Set up widgets for job posting

--- lib/form/doctrine/PluginsbJobBoardJobForm.class.php
+++ lib/form/doctrine/PluginsbJobBoardJobForm.class.php
@@ -10,4 +10,28 @@
  */
 abstract class PluginsbJobBoardJobForm extends BasesbJobBoardJobForm
 {
+	public function setup() 
+	{
+    parent::setup();
+		
+		$this->setWidget('author_id', new sfWidgetFormInputHidden(array(), array('value' => sfContext::getInstance()->getUser()->getGuardUser()->getId())));
+		
+		// Tags 
+		$options['default'] = implode(', ', $this->getObject()->getTags()); 
+		if (sfConfig::get('app_a_all_tags', true)) 
+		{ 
+			$options['all-tags'] = PluginTagTable::getAllTagNameWithCount(); 
+		} 
+
+		else 
+		{ 
+			sfContext::getInstance()->getConfiguration()->loadHelpers('Url'); 
+			$options['typeahead-url'] = url_for('taggableComplete/complete'); 
+		} 
+
+		$options['popular-tags'] = PluginTagTable::getPopulars(null, array(), false); 
+		$this->setWidget('tags', new pkWidgetFormJQueryTaggable($options, array('class' => 'tags-input'))); 
+		$this->setValidator('tags', new sfValidatorString(array('required' => false))); 
+		unset($this['created_at'], $this['updated_at']);
+	}
 }
