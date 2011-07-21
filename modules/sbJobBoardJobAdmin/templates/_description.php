0b436544c73e2d6f60d09b192989f12de36718d9
Giles Smith <giles@superrb.com>
2011-07-21 13:12:51 +0100

    Added a title to the job description

--- modules/sbJobBoardJobAdmin/templates/_description.php
+++ modules/sbJobBoardJobAdmin/templates/_description.php
@@ -8,10 +8,12 @@
 $sbJobBoardJob = $form->getObject();
 $edit = true;
 ?>
-<?php var_dump($sbJobBoardJob->getSlug()); ?>
+<hr/>
+<h3 class="job-board-description">Job Description</h3>
 <?php a_area('jobDescription', array(
 	  'edit' => $edit, 'toolbar' => 'basic', 'slug' => url_for('@sb_job_board_job_page?slug=' . $sbJobBoardJob->getSlug()),
 	  'allowed_types' => array('aRichText'),
 	  'type_options' => array(
 	    'aRichText' => array('tool' => 'Main'),
 	))) ?>
+<hr/>
