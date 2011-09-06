e57835bcd0e05528b480d909e169ae22c0c45fcb
Giles Smith <giles@superrb.com>
2011-09-06 10:48:05 +0100

    Added classes and titles to fields

--- lib/form/doctrine/PluginsbJobBoardApplicationForm.class.php
+++ lib/form/doctrine/PluginsbJobBoardApplicationForm.class.php
@@ -13,10 +13,10 @@ abstract class PluginsbJobBoardApplicationForm extends BasesbJobBoardApplication
 	public function configure()
 	{
 		$this->setWidgets(array(
-			'name' => new sfWidgetFormInputText(array('default' => 'Name')),
-			'email' => new sfWidgetFormInputText(array('default' => 'Email address')),
-			'phone_number' => new sfWidgetFormInputText(array('default' => 'Phone number')),
-			'job_type' => new sfWidgetFormInputText(array('default' => 'Job type')),
+			'name' => new sfWidgetFormInputText(array('default' => 'Name'), array('title' => 'Please enter your name', 'class' => 'application_name')),
+			'email' => new sfWidgetFormInputText(array('default' => 'Email address'),  array('title' => 'Please enter your email address', 'class' => 'application_email')),
+			'phone_number' => new sfWidgetFormInputText(array('default' => 'Phone number'),  array('title' => 'Please enter your phone number', 'class' => 'application_phone_number')),
+			'job_type' => new sfWidgetFormInputText(array('default' => 'Job type'),  array('title' => 'Please describe what jobs interest you', 'class' => 'application_job_type')),
 			'cv_file' => new sfWidgetFormInputFile(array('label' => 'Upload CV'))
 		));
 
