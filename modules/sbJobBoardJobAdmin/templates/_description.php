d65f8cab5a08315a9eb258eed20f5c0eaeb970a1
Giles Smith <giles@superrb.com>
2011-09-07 14:22:26 +0100

    Use true page slug

--- modules/sbJobBoardJobAdmin/templates/_description.php
+++ modules/sbJobBoardJobAdmin/templates/_description.php
@@ -5,13 +5,12 @@
  * and open the template in the editor.
  */
 
-$sbJobBoardJob = $form->getObject();
 $edit = true;
 ?>
 <hr/>
 <h3 class="job-board-description">Job Description</h3>
 <?php a_area('jobDescription', array(
-	  'edit' => $edit, 'toolbar' => 'basic', 'slug' => url_for('@sb_job_board_job_page?slug=' . $sbJobBoardJob->getSlug()),
+	  'edit' => $edit, 'toolbar' => 'basic', 'slug' => aPageTable::getFirstEnginePage('sbJobBoardJob')->slug . "/" . $form->getObject()->getSlug(),
 	  'allowed_types' => array('aRichText'),
 	  'type_options' => array(
 	    'aRichText' => array('tool' => 'Main'),
